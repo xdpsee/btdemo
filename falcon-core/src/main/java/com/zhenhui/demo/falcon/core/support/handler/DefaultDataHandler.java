b613493cc0695ec10ec3270e65cfc738bd497091
zhenhui.czh <zhenhui.czh@alibaba-inc.com>
2018-05-23 23:49:17 +0800

    :

--- falcon-core/src/main/java/com/zhenhui/demo/falcon/core/support/handler/DefaultDataHandler.java
+++ falcon-core/src/main/java/com/zhenhui/demo/falcon/core/support/handler/DefaultDataHandler.java
@@ -2,8 +2,12 @@ package com.zhenhui.demo.falcon.core.support.handler;
 
 import com.zhenhui.demo.falcon.core.domain.Position;
 import com.zhenhui.demo.falcon.core.server.ServerConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DefaultDataHandler extends AbstractDataHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(DefaultDataHandler.class);
 
     public DefaultDataHandler(ServerConnector connector) {
         super(connector);
@@ -17,7 +21,8 @@ public class DefaultDataHandler extends AbstractDataHandler {
                 positionService().savePosition(position);
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("DefaultDataHandler.savePosition exception", e);
+            throw e;
         }
 
         return position;
